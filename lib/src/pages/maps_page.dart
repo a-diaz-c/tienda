8ce3ceda62d76391f5d47f1a1445b36f0661ada2
Daniela Barrientos <daniela@hotmail.com>
2020-10-08 13:03:33 -0500

    sse marco las tiendas en mapa

--- lib/src/pages/maps_page.dart
+++ lib/src/pages/maps_page.dart
@@ -29,95 +29,129 @@ class _MapasPageState extends State<MapasPage> {
     WidgetsFlutterBinding.ensureInitialized();
   }
 
-  /* static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(19.4978, -99.1269),
-    zoom: 5.3,
-  ); */
-
-  /*  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(16.8638, -99.8816),
-      tilt: 59.440717697143555,
-      zoom: 13.00); */
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: GoogleMap(
-              key: _key,
-              markers: {
-                Marker(
-                  GeoCoord(34.0469058, -118.3503948),
-                ),
-                Marker(
-                  GeoCoord(16.8638, -99.8816),
-                ),
-              },
-              initialZoom: 5.6,
-              initialPosition: GeoCoord(19.4978, -99.1269),
-              mobilePreferences: const MobileMapPreferences(
-                trafficEnabled: true,
-                zoomControlsEnabled: false,
-              ),
-              webPreferences: WebMapPreferences(
-                fullscreenControl: true,
-                zoomControl: true,
-              ),
-            ),
-          ),
-          Positioned(
-            top: 16,
-            left: 16,
-            child: FloatingActionButton(
-              child: Icon(Icons.person_pin_circle),
-              onPressed: () {
-                final bounds = GeoCoordBounds(
-                  northeast: GeoCoord(16.8638, -99.8816),
-                  southwest: GeoCoord(15.8638, -99.8816),
-                );
-                GoogleMap.of(_key).moveCameraBounds(bounds);
-                /* GoogleMap.of(_key).addMarkerRaw(
-                  GeoCoord(
-                    (bounds.northeast.latitude + bounds.southwest.latitude) / 2,
-                    (bounds.northeast.longitude + bounds.southwest.longitude) /
-                        2,
-                  ),
-                  onTap: (markerId) async {
-                    await showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        content: Text(
-                          'This dialog was opened by tapping on the marker!\n'
-                          'Marker ID is $markerId',
+        body: ListView(
+          children: [
+            Navbar(),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.50,
+              width: MediaQuery.of(context).size.width * 0.50,
+              child: GoogleMap(
+                key: _key,
+                markers: {
+                  Marker(
+                    GeoCoord(18.934104, -99.197179),
+                    onTap: (markerId) async {
+                      await showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          content: Text("Applebee's Cuernavaca"),
+                          actions: <Widget>[
+                            FlatButton(
+                              onPressed: Navigator.of(context).pop,
+                              child: Text('CLOSE'),
+                            ),
+                          ],
                         ),
-                        actions: <Widget>[
-                          FlatButton(
-                            onPressed: Navigator.of(context).pop,
-                            child: Text('CLOSE'),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ); */
-              },
+                      );
+                    },
+                  ),
+                  Marker(
+                    GeoCoord(16.8638, -99.8816),
+                    onTap: (markerId) async {
+                      await showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          content: Text("Applebee's Acapulco"),
+                          actions: <Widget>[
+                            FlatButton(
+                              onPressed: Navigator.of(context).pop,
+                              child: Text('CLOSE'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  Marker(
+                    GeoCoord(19.487504, -99.153238),
+                    onTap: (markerId) async {
+                      await showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          content: Text("Applebee's Vía Vallejo"),
+                          actions: <Widget>[
+                            FlatButton(
+                              onPressed: Navigator.of(context).pop,
+                              child: Text('CLOSE'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  Marker(
+                    GeoCoord(19.442545, -99.204091),
+                    onTap: (markerId) async {
+                      await showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          content: Text("Applebee’s Plaza Carso"),
+                          actions: <Widget>[
+                            FlatButton(
+                              onPressed: Navigator.of(context).pop,
+                              child: Text('CLOSE'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  Marker(
+                    GeoCoord(25.457571, -100.979277),
+                    onTap: (markerId) async {
+                      await showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          content: Text("Applebee´s Galerías Saltillo"),
+                          actions: <Widget>[
+                            FlatButton(
+                              onPressed: Navigator.of(context).pop,
+                              child: Text('CLOSE'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                },
+                initialZoom: 5.6,
+                initialPosition: GeoCoord(19.4978, -99.1269),
+                mobilePreferences: const MobileMapPreferences(
+                  trafficEnabled: true,
+                  zoomControlsEnabled: false,
+                ),
+                webPreferences: WebMapPreferences(
+                  fullscreenControl: true,
+                  zoomControl: true,
+                ),
+              ),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          final bounds = GeoCoordBounds(
-            northeast: GeoCoord(16.8638, -99.8816),
-            southwest: GeoCoord(15.8638, -99.8816),
-          );
-          GoogleMap.of(_key).moveCameraBounds(bounds);
-        },
-        label: Text('Acapulco'),
-        icon: Icon(Icons.person_pin_circle),
-      ),
-    );
+            footer(),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            final bounds = GeoCoordBounds(
+              northeast: GeoCoord(16.8638, -99.8816),
+              southwest: GeoCoord(15.8638, -99.8816),
+            );
+            GoogleMap.of(_key).moveCameraBounds(bounds);
+          },
+          label: Text('Acapulco'),
+          icon: Icon(Icons.person_pin_circle),
+        ));
   }
 }
